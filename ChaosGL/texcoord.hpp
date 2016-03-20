1a0fa578c7f7f22f83ad33c867d5b4de6f2df8f7
Fu Lam Diep <f.diep@netzbewegung.com>
2016-03-20 21:15:39 +0100

    [Add] Attribute type for texcoord

---
+++ ChaosGL/texcoord.hpp
@@ -0,0 +1,19 @@
+//
+//  texcoord.hpp
+//  ChaosGL
+//
+//  Created by Fu Lam Diep on 20.03.16.
+//  Copyright © 2016 Fu Lam Diep. All rights reserved.
+//
+
+#ifndef ChaosGL_texcoord_h
+#define ChaosGL_texcoord_h
+
+#include "ttexcoord.hpp"
+
+namespace chaosgl
+{
+	
+}
+
+#endif /* texcoord_h */
---
+++ ChaosGL/ttexcoord.hpp
@@ -0,0 +1,24 @@
+//
+//  ttexcoord.hpp
+//  ChaosGL
+//
+//  Created by Fu Lam Diep on 20.03.16.
+//  Copyright © 2016 Fu Lam Diep. All rights reserved.
+//
+
+#ifndef ChaosGL_ttexcoord_h
+#define ChaosGL_ttexcoord_h
+
+#include <glm/glm.hpp>
+#include "attrib.hpp"
+
+template<typename V>
+struct ttexcoord
+{
+public:
+	typedef ttexcoord<V> Self;
+	
+	typedef V VertexType;
+};
+
+#endif /* ttexcoord_h */
